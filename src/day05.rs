d818a049383ed2e1ee95bf093a127a4c3dc8c5d5
Kyle Harris <kaharrs2@gmail.com>
2019-12-06 19:53:27 -0700

    fixed day 5

--- src/day05.rs
+++ src/day05.rs
@@ -9,7 +9,7 @@ pub fn load_input() -> Vec<i64> {
     f.read_to_string(&mut buffer).unwrap();
     let mut output = Vec::new();
     for el in buffer.split(",") {
-        if let Ok(x) = el.parse::<i64>() {
+        if let Ok(x) = el.trim().parse::<i64>() {
             output.push(x)
         }
     }
@@ -49,173 +49,116 @@ pub fn run(input: i64, memory: &Vec<i64>) -> i64 {
 
         match opcode {
             1 => {
-                println!(
-                    "Add - {}, {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2],
-                    input_copy[ic + 3]
-                );
                 // Add
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
-                println!("A mode: {}", mode);
                 let value_a = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("A value: {}", value_a);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
-                println!("B mode: {}", mode);
                 let value_b = parse_param(input_copy[ic + 2], mode, &mut input_copy);
-                println!("B value: {}", value_b);
 
                 // Destination mode must always be position
                 let c = input_copy[ic + 3] as usize;
                 let result = value_a + value_b;
-                println!("A + B = {}", result);
                 input_copy[c] = result;
                 ic += 4;
             }
             2 => {
-                println!(
-                    "Multiply - {}, {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2],
-                    input_copy[ic + 3]
-                );
                 // Multiply
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let value_a = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("a: {}", value_a);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
                 let value_b = parse_param(input_copy[ic + 2], mode, &mut input_copy);
-                println!("b: {}", value_b);
 
                 // Destination mode must always be position
                 let c = input_copy[ic + 3] as usize;
                 let result = value_a * value_b;
-                println!("A * B = {}", result);
                 input_copy[c] = result;
                 ic += 4;
             }
             3 => {
-                println!("Store - {}, {}", input_copy[ic], input_copy[ic + 1]);
                 // Store input in memory
                 let a = input_copy[ic + 1] as usize;
-                println!("ia: {}", a);
                 input_copy[a] = input;
                 ic += 2;
             }
             4 => {
-                println!("Load - {}, {}", input_copy[ic], input_copy[ic + 1]);
                 // Load output from memory
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let value_a = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("a: {}", value_a);
                 output = value_a;
-                println!("\noutput: {}\n", output);
                 ic += 2;
             }
             5 => {
-                println!(
-                    "Jump if True - {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2]
-                );
                 // Jump if true
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let p1 = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("p1: {}", p1);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
                 let p2 = parse_param(input_copy[ic + 2], mode, &mut input_copy) as usize;
-                println!("p2: {}", p2);
 
                 if p1 != 0 {
-                    println!("Jump");
                     ic = p2;
                 } else {
-                    println!("Don't Jump");
                     ic += 3;
                 }
             }
             6 => {
-                println!(
-                    "Jump if False - {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2]
-                );
                 // Jump if false
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let p1 = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("p1: {}", p1);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
                 let p2 = parse_param(input_copy[ic + 2], mode, &mut input_copy) as usize;
-                println!("p2: {}", p2);
 
                 if p1 == 0 {
-                    println!("Jump");
                     ic = p2;
                 } else {
-                    println!("Don't Jump");
                     ic += 3;
                 }
             }
             7 => {
-                println!(
-                    "Less Than - {}, {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2],
-                    input_copy[ic + 3]
-                );
                 // Less than
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let p1 = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("p1: {}", p1);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
                 let p2 = parse_param(input_copy[ic + 2], mode, &mut input_copy);
-                println!("p2: {}", p2);
 
                 let p3 = input_copy[ic + 3] as usize;
-                println!("p3: {}", p3);
 
                 if p1 < p2 {
                     input_copy[p3] = 1;
@@ -225,30 +168,20 @@ pub fn run(input: i64, memory: &Vec<i64>) -> i64 {
                 ic += 4;
             }
             8 => {
-                println!(
-                    "Equals - {}, {}, {}, {}",
-                    input_copy[ic],
-                    input_copy[ic + 1],
-                    input_copy[ic + 2],
-                    input_copy[ic + 3]
-                );
                 // Equals
                 let mut mode = 0;
                 if imarr.len() > 0 {
                     mode = imarr[imarr.len() - 1];
                 }
                 let p1 = parse_param(input_copy[ic + 1], mode, &mut input_copy);
-                println!("p1: {}", p1);
 
                 mode = 0;
                 if imarr.len() > 1 {
                     mode = imarr[imarr.len() - 2];
                 }
                 let p2 = parse_param(input_copy[ic + 2], mode, &mut input_copy);
-                println!("p2: {}", p2);
 
                 let p3 = input_copy[ic + 3] as usize;
-                println!("p3: {}", p3);
 
                 if p1 == p2 {
                     input_copy[p3] = 1;
@@ -258,7 +191,6 @@ pub fn run(input: i64, memory: &Vec<i64>) -> i64 {
                 ic += 4;
             }
             99 => {
-                println!("Final output: {}", output);
                 return output;
             }
             _ => panic!("Unknown opcode: {:?}", opcode),
